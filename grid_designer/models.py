f5b5c93c29948d511d0023454a74b3bf452a1514
Raphael Santana <uroboros.phael@gmail.com>
2019-06-15 14:35:52 -0300

    Adding grounding mesh model.

--- grid_designer/models.py
+++ grid_designer/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class GroundingMesh(models.Model):
+    fault_current = models.FloatField()
+    trip_time = models.FloatField()
+    grid_depth = models.FloatField()
+    grid_height = models.FloatField()
+    grid_width = models.FloatField()
+    ground_resistivity = models.FloatField()
+    gravel_depth = models.FloatField()
+    gravel_resistivity = models.FloatField()
+    room_temperature = models.FloatField()
+    maximum_temperature = models.FloatField()
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
